7a7362a10837104ae98e91a0af9f2f2f0ffcbcdc
Bruno <38665405+TheBrunoRM@users.noreply.github.com>
2025-12-21 05:33:42 -0300

    fix message sounds error

--- src/main/java/me/thebrunorm/skywars/events/MessageSound.java
+++ src/main/java/me/thebrunorm/skywars/events/MessageSound.java
@@ -1,24 +1,29 @@
 /* (C) 2021 Bruno */
 package me.thebrunorm.skywars.events;
 
+import mrblobman.sounds.Sounds;
 import org.bukkit.Bukkit;
+import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 
 import me.thebrunorm.skywars.Skywars;
-import mrblobman.sounds.Sounds;
 
 public class MessageSound implements Listener {
 
-	String sound = Skywars.config.getString("messageSounds.sound");
-
 	@EventHandler
 	void onMessage(AsyncPlayerChatEvent event) {
+		String soundName = Skywars.get().getConfig().getString("messageSounds.sound");
+		Sound sound = Sounds.valueOf(soundName).bukkitSound();
+		if(sound == null) {
+			Skywars.get().getLogger().severe("[Message Sounds] Could not find sound: " + soundName);
+			return;
+		}
 		final float random = (float) (Math.random() + 0.5f);
 		for (final Player player : Bukkit.getOnlinePlayers()) {
-			player.playSound(player.getLocation(), Sounds.valueOf(this.sound).bukkitSound(), 1, random);
+			player.playSound(player.getLocation(), sound, 1, random);
 		}
 	}
 
