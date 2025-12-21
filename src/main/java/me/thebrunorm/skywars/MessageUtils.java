986ef8716f87d737dddb1bf6f370f652b3060558
Nyanyagulugulu <qianxun114514@gmail.com>
2025-12-22 02:26:22 +0800

    仓库合并

---
+++ src/main/java/me/thebrunorm/skywars/MessageUtils.java
@@ -0,0 +1,55 @@
+/* (C) 2021 Bruno */
+package me.thebrunorm.skywars;
+
+import me.thebrunorm.skywars.structures.Arena;
+import me.thebrunorm.skywars.structures.SkywarsUser;
+import org.bukkit.ChatColor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import java.util.List;
+
+class MessageUtils {
+
+	public static final char ALT_COLOR_CHAR = '&';
+
+	public static void send(CommandSender sender, String text, Object... format) {
+		sender.sendMessage(color(text, format));
+	}
+
+	public static void sendTranslated(CommandSender sender, String key, Object... format) {
+		sender.sendMessage(getMessage(key, format));
+	}
+
+	public static String color(String text) {
+		return ChatColor.translateAlternateColorCodes(ALT_COLOR_CHAR, text);
+	}
+
+	public static String color(String text, Object... format) {
+		return MessageUtils.color(String.format(text, format));
+	}
+
+	public static String get(String name, Object... format) {
+		return getMessage(name, format);
+	}
+
+	public static String getMessage(String name, Object... format) {
+		String msg = "";
+		if (Skywars.langConfig.get(name) instanceof List)
+			msg = String.join("\n", Skywars.langConfig.getStringList(name));
+		else
+			msg = Skywars.langConfig.getString(name);
+		if (msg == null) return name;
+		if (msg.isEmpty()) return "";
+		for (int i = 0; i < format.length; i++) {
+			msg = msg.replaceAll(String.format("\\{%s\\}", i), String.valueOf(format[i]));
+		}
+		return MessageUtils.color(msg);
+	}
+
+	public static String getFormattedMessage(String name, Player player, Arena arena, SkywarsUser swp,
+			Object... format) {
+		return MessageUtils.color(SkywarsUtils.format(getMessage(name, format), player, arena, swp));
+	}
+
+}
---
+++ src/main/java/me/thebrunorm/skywars/structures/ArenaEventManager.java
@@ -0,0 +1,53 @@
+package me.thebrunorm.skywars.structures;
+
+import me.thebrunorm.skywars.ArenaStatus;
+import me.thebrunorm.skywars.Messager;
+import me.thebrunorm.skywars.Skywars;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class ArenaEventManager {
+
+	private final Arena arena;
+	private final Queue<SkywarsEvent> events = new LinkedList<>();
+
+	public ArenaEventManager(Arena arena) {
+		this.arena = arena;
+		this.events.add(new SkywarsEvent(arena, SkywarsEventType.REFILL, 60));
+		this.events.add(new SkywarsEvent(arena, SkywarsEventType.REFILL, 60));
+		this.events.add(new SkywarsEvent(arena, SkywarsEventType.ENDER_DRAGON, 60 * 3));
+	}
+
+	public String getNextEventText() {
+		if (arena.getStatus() == ArenaStatus.RESTARTING)
+			return Skywars.langConfig.getString("status.ended");
+		final SkywarsEvent event = this.getNextEvent();
+		if (event == null)
+			return Messager.getMessage("events.no_event");
+		final int time = event.getTime();
+		final int minutes = time / 60;
+		final int seconds = time % 60;
+		final String timeString = String.format("%d:%02d", minutes, seconds);
+		final String eventNameKey = String.format("events.%s.name", event.getType().name());
+		return Messager.color(Skywars.langConfig.getString("events.format", "<events.format>")
+			.replaceAll("%name%", Skywars.langConfig.getString(eventNameKey, String.format("<%s>", eventNameKey)))
+			.replaceAll("%time%", timeString));
+
+	}
+
+	public SkywarsEvent getNextEvent() {
+		return events.peek();
+	}
+
+	public SkywarsEvent executeEvent() {
+		SkywarsEvent event = events.poll();
+		if (event == null) return null;
+		event.run();
+		return event;
+	}
+
+	public Queue<SkywarsEvent> getEvents() {
+		return events;
+	}
+}
