d6a0c2a2160733e8f20d6a8067ab2af375b4f206
Bruno <38665405+TheBrunoRM@users.noreply.github.com>
2025-12-22 11:02:12 -0300

    reorder inventory manager

--- src/main/java/me/thebrunorm/skywars/menus/PlayerInventoryManager.java
+++ src/main/java/me/thebrunorm/skywars/menus/PlayerInventoryManager.java
@@ -1,19 +1,26 @@
-/* (C) 2021 Bruno */
+// Copyright (c) 2025 Bruno
 package me.thebrunorm.skywars.menus;
 
-import java.util.HashMap;
-import java.util.UUID;
-
+import me.thebrunorm.skywars.Skywars;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryCloseEvent;
 
-import me.thebrunorm.skywars.Skywars;
+import java.util.HashMap;
+import java.util.UUID;
 
 public class PlayerInventoryManager implements Listener {
 
 	private static final HashMap<UUID, MenuType> currentMenu = new HashMap<>();
+
+	@EventHandler
+	void onInventoryClose(InventoryCloseEvent event) {
+		final Player player = (Player) event.getPlayer();
+		if (getCurrentMenu(player) == null) return;
+		Skywars.get().sendDebugMessage("inventory closed: " + event.getPlayer().getName());
+		setMenu(player, null);
+	}
 
 	public static MenuType getCurrentMenu(Player player) {
 		return currentMenu.get(player.getUniqueId());
@@ -22,14 +29,5 @@ public class PlayerInventoryManager implements Listener {
 	public static void setMenu(Player player, MenuType menu) {
 		Skywars.get().sendDebugMessage("inventory set to " + menu + ": " + player.getName());
 		currentMenu.put(player.getUniqueId(), menu);
-	}
-
-	@EventHandler
-	void onInventoryClose(InventoryCloseEvent event) {
-		final Player player = (Player) event.getPlayer();
-		if (getCurrentMenu(player) == null)
-			return;
-		Skywars.get().sendDebugMessage("inventory closed: " + event.getPlayer().getName());
-		setMenu(player, null);
 	}
 }
